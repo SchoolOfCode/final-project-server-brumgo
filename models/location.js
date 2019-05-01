f58157a0e103f59cf19a552e5f340da39b65a992
Olivia Winteringham <owinteringham@gmail.com>
2019-05-01 17:04:11 +0100

    inital commit

---
+++ models/location.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const locationsSchema = new mongoose.Schema({
+  id: String,
+  locationName: String,
+  category: String,
+  location: {
+    type: { type: String, enum: ["Point"], required: true },
+    coordinates: { type: [Number], required: true }
+  },
+  trivia: String,
+  imageUrl: String,
+  audioURL: String,
+  videoURL: String
+});
+
+module.exports = mongoose.model("Location", locationsSchema);
